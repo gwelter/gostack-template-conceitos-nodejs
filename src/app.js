dfd2096f85feeaaf2eb6acc04a5cd7b7a15b15e6
Guilherme André Welter <guilherme.a.welter@gmail.com>
2020-04-18 16:27:43 -0300

    Passing all tests

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,55 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { body } = request;
+  const newRepository = {
+    ...body,
+    id: uuid(),
+    likes: 0
+  }
+  repositories.push(newRepository);
+  return response.status(201).json(newRepository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const repositoryIndex = findRepositoryIndex(request.params.id);
+  if (repositoryIndex >= 0) {
+    const repository = repositories[repositoryIndex];
+    repositories[repositoryIndex] = {
+      ...repository,
+      ...request.body,
+      id: repository.id,
+      likes: repository.likes,
+    }
+    return response.json(repositories[repositoryIndex]);
+  }
+  return response.status(400).end();
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const repositoryIndex = findRepositoryIndex(request.params.id);
+  if (repositoryIndex >= 0) {
+    repositories.splice(repositoryIndex, 1);
+    return response.status(204).end();
+  }
+  return response.status(400).end();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const repositoryIndex = findRepositoryIndex(request.params.id);
+  if (repositoryIndex >= 0) {
+    repositories[repositoryIndex].likes += 1;
+    return response.json(repositories[repositoryIndex]);
+  }
+  return response.status(400).end();
 });
+
+function findRepositoryIndex(id) {
+  return repositories.findIndex(repo => repo.id === id)
+}
 
 module.exports = app;
